523a9e62433d7b1f8811c19c47ba1bc85c45306b
EC2 Default User <156569903+Karen136i@users.noreply.github.com>
2024-04-10 14:20:54 +0000

    public/items

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,8 +1,20 @@
 class Public::ItemsController < ApplicationController
   
   def index
+    @items = Item.page(params[:page]).per(12)
+    @categories = Category.all
+    @total_items = @items.total_count
   end
 
   def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:category_id, :introduction, :image, :price, :status)
+  end 
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   enum status: { on_sale: 0, not_on_sale: 1, back_orderd: 2 }
   has_one_attached :image
-  belongs_to :category, optional: true
+  belongs_to :category
   has_many :cart_items
 
   validates :category, presence: { message: "を選択してください" }
@@ -9,6 +9,11 @@ class Item < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
   validates :image, presence: true
   validates :introduction, presence: true, length: { maximum: 70 }
+  
+  def price_without_tax
+    tax_rate = 0.1 # 仮の税率（10%）
+    (price * (1 - tax_rate)).round
+  end
 
   scope :search, ->(query) {
     where("name LIKE ?", "%#{query}%") if query.present?
