b5846cc189a3fa9249da6b7078934b5db1fa4f6b
EC2 Default User <156569903+Karen136i@users.noreply.github.com>
2024-04-28 03:56:35 +0000

    微調整

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < ApplicationController
 
   def top
+    @items = Item.where(status:"on_sale")
     @random_items = if Rails.env == "development"
                        Item.order("RANDOM()").limit(4)  # ランダムに4つのアイテムを選択
                     elsif Rails.env == "production"
