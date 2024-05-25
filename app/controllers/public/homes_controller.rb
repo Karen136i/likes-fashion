e4b39008a2fdd354b8c9cccebb45bb4b0d91f689
EC2 Default User <156569903+Karen136i@users.noreply.github.com>
2024-05-25 05:31:02 +0000

    改善点の修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,18 +2,11 @@ class Public::HomesController < ApplicationController
 
   def top
     @items = Item.on_sale
-      @random_items = if Rails.env == "development"
-                        Item.on_sale.order("RANDOM()").limit(4)  # ランダムに4つのアイテムを選択
-                      elsif Rails.env == "production"
-                        Item.on_sale.order("RAND()").limit(4)
-                      end
+    @random_items = if Rails.env == "development"
+                       Item.on_sale.order("RANDOM()").limit(4)  # ランダムに4つのアイテムを選択
+                    elsif Rails.env == "production"
+                       Item.on_sale.order("RAND()").limit(4)
+                    end
   end
-
-  def about
-  end
-
-  private
-
-  def prepare_items
-  end
+  
 end
